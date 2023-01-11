61f10ec75aa3b6372d741fbccacd22b8e38062b6
Adarsh Dubey <112717088+D-Adarsh@users.noreply.github.com>
2023-01-11 18:34:02 +0530

    Add files via upload
    A simple * pattern program that can make square and rectangle depending on the users input

---
+++ Pattern1.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std; 
+
+/*
+
+OUTPUT :
+****
+****
+****
+****
+
+*/
+
+int main()
+{
+    //USING FOR LOOP
+    /*
+    int i, j, n;
+    cout<<"Enter the size of the square : "<<endl;
+    cin>>n;
+    for ( i = 0; i < n; i++)
+    {
+        for ( j = 0; j < n; j++)
+        {
+            cout<<'*';
+        }
+        cout<<endl;
+    }
+    */
+
+    //USING WHILE LOOP
+    /*
+    int n;
+    cin>>n;
+    int i = 0;
+    while (i<n)
+    {
+        int j = 0;
+        while (j<n)
+        {
+            cout<<'*';
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    */
+
+    return 0;
+}
